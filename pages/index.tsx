c9d9a1f2ab4342824acfb6a3f6bd598efbb87858
Mouad Bouras <mouad.bouras@cae.com>
2022-06-26 10:28:55 -0400

    sla7t l mouchkil

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <h2> Salut hiba</h2>
 
         <p className={styles.description}>
           Get started by editing{" "}
