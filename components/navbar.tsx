a9c570b0ed3a200a8fe512d1507dcfd65ad8fd8a
hebaabbadi <106325252+hebaabbadi@users.noreply.github.com>
2022-07-28 14:05:59 +0100

    ladaptation de html et css de la page nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,31 +1,46 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useContext } from "react";
-import { UserContext } from "../lib/context";
-import { auth } from "../lib/firebase";
-import { UserType } from "../lib/models/user-type.enum";
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useContext } from 'react'
+import { UserContext } from '../lib/context'
+import { auth } from '../lib/firebase'
+import { UserType } from '../lib/models/user-type.enum'
+import styles from '../styles/Navbar.module.scss'
 
 export default function Navbar() {
-  const user = useContext(UserContext);
-  const router = useRouter();
+  const user = useContext(UserContext)
+  const router = useRouter()
 
-  return (
-    router.route !== "/login" && (
-      <nav>
-        <ul className="nav mt-1">
-          <li className="nav-item">
-            <Link href="/">
-              <button type="button" className="btn btn-link">
-                Feed
-              </button>
-            </Link>
+  return router.route === '/login' ? (
+    <></>
+  ) : (
+    <nav id="sidebarMenu" className={styles.sidebar}>
+      <div className="position-sticky pt-3 sidebar-sticky">
+        <ul className="nav flex-column">
+          <li className={styles.navitem}>
+            <a className={styles.navlink} aria-current="page" href="/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="feather feather-home align-text-bottom "
+                aria-hidden="true"
+              >
+                <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                <polyline points="9 22 9 12 15 12 15 22"></polyline>
+              </svg>{' '}
+              <p className={styles.p}>Feed</p>
+            </a>
           </li>
           {!user && (
             <li className="nav-item">
               <Link href="/login">
-                <button type="button" className="btn btn-link">
-                  Login
-                </button>
+                <p className={styles.p1}>Login</p>
               </Link>
             </li>
           )}
@@ -33,25 +48,73 @@ export default function Navbar() {
           {user && user.userType === UserType.Admin && (
             <>
               <li className="nav-item">
-                <Link href="/users/new">
-                  <button type="button" className="btn btn-link">
-                    Create User
-                  </button>
-                </Link>
+                <a className={styles.navlink} href="/users/new">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="feather feather-file align-text-bottom"
+                    aria-hidden="true"
+                  >
+                    <path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path>
+                    <polyline points="13 2 13 9 20 9"></polyline>
+                  </svg>
+
+                  <p className={styles.p}> Create User</p>
+                </a>
               </li>
               <li className="nav-item">
-                <Link href="/users">
-                  <button type="button" className="btn btn-link">
-                    User List
-                  </button>
-                </Link>
+                <a className={styles.navlink} href="/users">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="feather feather-users align-text-bottom"
+                    aria-hidden="true"
+                  >
+                    <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                    <circle cx="9" cy="7" r="4"></circle>
+                    <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
+                    <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+                  </svg>
+
+                  <p className={styles.p}>User List</p>
+                </a>
               </li>
               <li className="nav-item">
-                <Link href="/courses">
-                  <button type="button" className="btn btn-link">
-                    Create Course
-                  </button>
-                </Link>
+                <a className={styles.navlink} href="/courses">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="feather feather-bar-chart-2 align-text-bottom"
+                    aria-hidden="true"
+                  >
+                    <line x1="18" y1="20" x2="18" y2="10"></line>
+                    <line x1="12" y1="20" x2="12" y2="4"></line>
+                    <line x1="6" y1="20" x2="6" y2="14"></line>
+                  </svg>
+
+                  <p className={styles.p}> Create Course</p>
+                </a>
               </li>
             </>
           )}
@@ -84,19 +147,16 @@ export default function Navbar() {
           {!!user && (
             <li className="nav-item">
               <Link href="/">
-                <button
-                  onClick={() => auth.signOut()}
-                  type="button"
-                  className="btn btn-link d-flex"
-                >
-                  logout &nbsp;
+                <p onClick={() => auth.signOut()} className={styles.p1}>
+                  {' '}
+                  Logout &nbsp;
                   <i className="bi bi-box-arrow-right"></i>
-                </button>
+                </p>
               </Link>
             </li>
           )}
         </ul>
-      </nav>
-    )
-  );
+      </div>
+    </nav>
+  )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,23 @@
-import type { AppProps } from "next/app";
-import { Toaster } from "react-hot-toast";
-import Navbar from "../components/navbar";
-import { UserContext } from "../lib/context";
-import { useUserData } from "../lib/hooks";
-import "../styles/globals.scss";
+import type { AppProps } from 'next/app'
+import { Toaster } from 'react-hot-toast'
+import Banner from '../components/Banner'
+import Navbar from '../components/navbar'
+import { UserContext } from '../lib/context'
+import { useUserData } from '../lib/hooks'
+import '../styles/globals.scss'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const user = useUserData();
-
+  const user = useUserData()
   return (
     <>
       <UserContext.Provider value={user}>
-        <Navbar></Navbar>
+        <Banner></Banner>
+         <Navbar></Navbar>
+
         <Component {...pageProps} />
+
         <Toaster></Toaster>
       </UserContext.Provider>
     </>
-  );
+  )
 }
